25de7e831527993e8cdb35fb0ad716e1fc9e531c
Farhat Abdad <farhat.a@live.com>
2015-01-19 15:00:19 +0100

    some stuff + homepage edit

--- views/docent.php
+++
@@ -1,2 +0,0 @@
-<h1 id="dinfo">Klik om verder te gaan</h1>
-<a href="?action=login"><button class="docentlogin">Login</button></a>
--- views/mainPage.php
+++ views/mainPage.php
@@ -2,13 +2,13 @@
 		<h1>De ToetsTool</h1>
 		<img id="logo" src="images/logo.png">
 		<div class="buttons">
-			<a href="?action=docent">
-			<div class="docentButton">
+			<a href="?action=login">
+			<div class="trans docentButton">
 				Docent
 			</div>
 			</a>
 			<a href="?action=leerling">
-			<div class="leerlingButton">
+			<div class="trans leerlingButton">
 				Leerling
 			</div>
 			</a>
